c25b4d51f26de3bf477c31481dba16ac88fbf497
07162262198 <andrelucasdasilva2005@gmail.com>
2026-08-28 15:38:35 -0300

    atividade 1

--- src/main/java/org/example/Main.java
+++ src/main/java/org/example/Main.java
@@ -4,16 +4,13 @@ package org.example;
 // click the <icon src="AllIcons.Actions.Execute"/> icon in the gutter.
 public class Main {
     static void main() {
+        // Declara uma variável do tipo int e atribui um valor
+        int numeroInt = 42;
 
-        //TIP Press <shortcut actionId="ShowIntentionActions"/> with your caret at the highlighted text
+// Ocorre o casting implícito (promoção automática de int para double)
+        double numeroDouble = numeroInt;
 
-        // to see how IntelliJ IDEA suggests fixing it.
-        IO.println(String.format("Hello and welcome!"));
-
-        for (int i = 1; i <= 5; i++) {
-            //TIP Press <shortcut actionId="Debug"/> to start debugging your code. We have set one <icon src="AllIcons.Debugger.Db_set_breakpoint"/> breakpoint
-            // for you, but you can always add more by pressing <shortcut actionId="ToggleLineBreakpoint"/>.
-            IO.println("i = " + i);
-        }
+// Exibe o valor da variável double no console
+        System.out.println("Valor em double: " + numeroDouble);
     }
 }
