9b4dbdec8938e8394ca552217db0647f163b6ef5
07162262198 <andrelucasdasilva2005@gmail.com>
2026-09-14 13:55:55 -0300

    FEAT-: ATIVIDADE 2

---
+++ src/main/java/org/example/producao.java
@@ -0,0 +1,20 @@
+package org.example;
+
+public class producao {
+
+        public static void main(String[] args) {
+
+            // Inicia o contador de peças
+            int peca = 1;
+
+            // Verifica se a peça é menor ou igual a 20
+            while (peca <= 20) {
+
+                // Exibe a mensagem de peça produzida
+                System.out.println("Peça Produzida");
+
+                // Incrementa o contador
+                peca++;
+            }
+        }
+}
